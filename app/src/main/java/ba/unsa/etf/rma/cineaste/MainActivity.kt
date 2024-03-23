6eddf5705ff326f9460f059bdbab0eca5c4587d9
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2024-03-23 22:40:06 +0100

    Setting RecyclerView

--- app/src/main/java/ba/unsa/etf/rma/cineaste/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/rma/cineaste/MainActivity.kt
@@ -2,10 +2,23 @@ package ba.unsa.etf.rma.cineaste
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var favoriteMovies: RecyclerView
+    private lateinit var favoriteMoviesAdapter: MovieListAdapter
+    private var favoriteMoviesList = getFavoriteMovies()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        favoriteMovies = findViewById(R.id.favoriteMovies)
+        favoriteMovies.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        favoriteMoviesAdapter = MovieListAdapter(listOf())
+        favoriteMovies.adapter = favoriteMoviesAdapter
+        favoriteMoviesAdapter.updateMovies(favoriteMoviesList)
     }
 }
